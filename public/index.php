c672bfd58ef94b08be3062849a5d457931430c8c
cris-ubuntu <cdcalderonrojas@gmail.com>
2025-02-20 17:38:55 +0100

    Las rutas ya funcionan

--- public/index.php
+++ public/index.php
@@ -1,21 +1,35 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// require_once __DIR__ . '/vendor/autoload.php';
 require_once "../vendor/autoload.php";
 
+// Configurar Twig con la carpeta de templates
+$templatesPath = realpath(__DIR__ . '/templates');
+if (!$templatesPath || !is_dir($templatesPath)) {
+    die("Error: La carpeta templates NO EXISTE en " . __DIR__ . '/templates');
+}
 
-// $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
-$loader = new \Twig\Loader\FilesystemLoader('./templates');
-
-// $twig = new \Twig\Environment($loader, [
-//     'cache' => __DIR__ . '/cache', // Opcional, para usar caché de Twig
-// ]);
-
+$loader = new \Twig\Loader\FilesystemLoader($templatesPath);
 $twig = new \Twig\Environment($loader, [
-    //	'cache' => 'cache',
-    ]);
+    //'cache' => __DIR__ . '/../cache',
+]);
 
+// Obtener la ruta actual desde $_GET['route'] (modificada por .htaccess)
+$request_uri = $_GET['route'] ?? 'home'; // Si no hay ruta, se carga "home"
 
-echo $twig->render('index.html.twig', ['name' => 'Cristian']);
+// Mapeo de rutas a sus respectivos archivos Twig
+$routes = [
+    'home' => 'home.html.twig',
+    'register' => 'register.html.twig',
+    'login' => 'login.html.twig'
+];
 
-?>
+// Verificar si la ruta existe en el array de rutas
+if (array_key_exists($request_uri, $routes)) {
+    echo $twig->render($routes[$request_uri]);
+} else {
+    // Si la ruta no existe, mostrar 404
+    http_response_code(404);
+    echo $twig->render('404.html.twig');
+}
